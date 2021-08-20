b8866e8bb98cca9829ac03b10792444d83492b5f
barizalhaq <bariz@qiscus.net>
2021-08-20 15:22:16 +0700

    hotfix

--- utils/agent/agent.go
+++ utils/agent/agent.go
@@ -2,6 +2,8 @@ package agent
 
 import (
 	"bot-routing-engine/entities/viewmodel"
+	"math/rand"
+	"time"
 )
 
 func GetAvailableRandomlyAgent(agents []viewmodel.Agent) (bool, viewmodel.Agent) {
@@ -13,9 +15,9 @@ func GetAvailableRandomlyAgent(agents []viewmodel.Agent) (bool, viewmodel.Agent)
 	}
 
 	if len(onlineAgents) > 0 {
-		// rand.Seed(time.Now().Unix())
-		// randomIndex := rand.Intn(len(onlineAgents))
-		return true, onlineAgents[0]
+		rand.Seed(time.Now().Unix())
+		randomIndex := rand.Intn(len(onlineAgents))
+		return true, onlineAgents[randomIndex]
 	}
 
 	return false, viewmodel.Agent{}
@@ -32,8 +34,8 @@ func GetDivisionByName(divisionName string, divisions []viewmodel.Division) view
 }
 
 func GetRandomAgent(agents []viewmodel.Agent) viewmodel.Agent {
-	// rand.Seed(time.Now().Unix())
-	// randomIndex := rand.Intn(len(agents))
+	rand.Seed(time.Now().Unix())
+	randomIndex := rand.Intn(len(agents))
 
-	return agents[0]
+	return agents[randomIndex]
 }
